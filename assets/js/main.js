4b0493fc70c0338252a024a6b78cc1bfd6add43f
Rodrigo <55567123+rodrigodasilv@users.noreply.github.com>
2021-05-20 16:00:24 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,53 +3,51 @@ function focusOnElement(where){
     elmnt.scrollIntoView(); 
 }
 
-function calcIMC(){
-    var peso = document.getElementById("peso").value;
-    var tamanho = document.getElementById("tamanho").value;
-    var element = "";
-    console.log(tamanho);
-    console.log(peso);
+function addClasses(){
+    element.classList.add("bold");
+    element.classList.add("uppercase");
+}
 
-    if(peso=="" || tamanho=="" || peso==0 || tamanho==0){
-        M.toast({html: 'Um (ou ambos) campo(s) não foram preenchido(s) corretamente!'});
-    }else{
-        var imc = peso / (tamanho*tamanho);
-        var imc = imc.toFixed(2);
-        M.toast({html: 'Seu IMC é '+ imc});
+let pessoa = {
+    peso: this.peso, 
+    altura: this.altura,  
+    imc: this.imc,
+    calcIMC :  function() {
+        this.peso = document.getElementById("peso").value;
+        this.altura = document.getElementById("altura").value;
 
-        // Tira o bold do IMC selecionado antes
-        var itemDaLista = 0;
-        for (; itemDaLista < 4; ) {
-            element = document.getElementById(itemDaLista);
-            element.classList.remove("bold");
-            element.classList.remove("uppercase");
-            itemDaLista++;
-        } 
+        if(this.peso=="" || this.altura=="" || this.peso==0 || this.altura==0){
+            M.toast({html: 'Um (os ambos) os campos não foram preenchidos corretamente!'});
+        }else{
+            this.imc = this.peso / (this.altura*this.altura);
+            this.imc = this.imc.toFixed(2);
+            M.toast({html: 'Seu IMC é '+ this.imc});
+            
+            for (itemDaLista=0; itemDaLista < 4; ) {
+                element = document.getElementById(itemDaLista);
+                element.classList.remove("bold");
+                element.classList.remove("uppercase");
+                itemDaLista++;
+            } 
 
-        // Função para adicionar as classes
-        function addClasses(){
-            element.classList.add("bold");
-            element.classList.add("uppercase");
-        }
-
-        // Adiciona as classes na categoria que o IMC se encaixa
-        if(imc<=18.49){
-            element = document.getElementById("0");
-            addClasses();
-        }else if(imc>=18.5 && imc<=24.99){
-            element = document.getElementById("1");
-            addClasses();
-        }else if(imc>=25 && imc<=29.9){
-            element = document.getElementById("2");
-            addClasses();
-        }else if(imc>=30 && imc<=39.99){
-            element = document.getElementById("3");
-            addClasses();
-        }else if(imc>39.99){
-            element = document.getElementById("4");
-            addClasses();
-        }
-        // Muda o foco para a tabela
-        focusOnElement('results');
+            if(this.imc<=18.49){
+                element = document.getElementById("0");
+                addClasses();
+            }else if(this.imc>=18.5 && this.imc<=24.99){
+                element = document.getElementById("1");
+                addClasses();
+            }else if(this.imc>=25 && this.imc<=29.9){
+                element = document.getElementById("2");
+                addClasses();
+            }else if(this.imc>=30 && this.imc<=39.99){
+                element = document.getElementById("3");
+                addClasses();
+            }else if(this.imc>39.99){
+                element = document.getElementById("4");
+                addClasses();
+            }
+            // Muda o foco para a tabela
+            focusOnElement('results');
     }
+}
 }
